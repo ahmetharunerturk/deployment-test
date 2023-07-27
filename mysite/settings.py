abad84efd03241a21745dc718276913c2c688afe
Ahmet Ertürk <is_ahmet@outlook.com>
2023-07-27 22:18:26 +0200

    changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'Togwl8RqcapX4kqVkgqw',
+        'HOST': 'containers-us-west-177.railway.app',
+        'PORT': '6431',   
     }
 }
 
