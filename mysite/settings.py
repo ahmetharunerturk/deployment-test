237a14a980f93a5779cf6df2e62440686b26b1b0
Ahmet Ertürk <is_ahmet@outlook.com>
2023-07-28 15:05:26 +0200

    database

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': 'Togwl8RqcapX4kqVkgqw',
+        'PASSWORD': 'PGPASSWORD',
         'HOST': 'containers-us-west-177.railway.app',
         'PORT': '6431',   
     }
