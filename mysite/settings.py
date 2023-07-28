63c734cd27c27493d2e6de7c83445a3bd4e30d76
Ahmet Ertürk <is_ahmet@outlook.com>
2023-07-28 11:24:04 +0200

    settings.py changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,6 +128,7 @@ import os
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Default primary key field type
