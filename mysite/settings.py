597f6ffb246a286a08158043fce38aadcd5a2c99
Ahmet Ertürk <is_ahmet@outlook.com>
2023-07-29 11:54:04 +0200

    settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['deployment-test-production-7e75.up.railway.app']
 
 
 # Application definition
